55f0109735a8e10e1b3bd6eca4e2faf5aa51a6e2
40197901 <owebb02@qub.ac.uk>
2020-11-18 13:16:03 +0000

    Adding Routing for Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { ConsentScreenComponent } from './components/consent-screen/consent-screen.component';
@@ -14,7 +15,8 @@ import { CircleTimeComponent } from './components/circle-time/circle-time.compon
     CircleTimeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
