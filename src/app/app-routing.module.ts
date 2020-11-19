94a4f5bc52148abd69149d0e361cb8df6539c503
40197901 <owebb02@qub.ac.uk>
2020-11-19 11:07:04 +0000

    Added some Html for Consent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ConsentScreenComponent } from './components/consent-screen/consent-screen.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'consent', component: ConsentScreenComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
 ];
 
