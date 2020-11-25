690f87fe320d415688701bed1197244a05843453
Stephen Patterson <spatterson43@qub.ac.uk>
2020-11-25 10:48:03 +0000

    Added drag and drop

--- src/app/components/circle-time/circle-time.component.ts
+++ src/app/components/circle-time/circle-time.component.ts
@@ -9,7 +9,14 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
 export class CircleTimeComponent implements OnInit {
 
   images = [
-    "../../../assets/teddy.png"
+    "../../../assets/teddy.png",
+    "../../../assets/pillow_blue.png",
+    "../../../assets/pillow_green.png",
+    "../../../assets/pillow_magenta.png",
+    "../../../assets/pillow_purple.png",
+    "../../../assets/pillow_red.png",
+    "../../../assets/pillow_turquoise.png",
+    "../../../assets/pillow_yellow.png"
   ];
 
   constructor() { }
